19118b7d3739d0875ae781e69cec0cdacb63d6b7
aorinngoDo <aorinngo@5x2.de>
2025-04-06 15:58:11 +0900

    update: get_sheeta_class_type util

--- utils.py
+++ utils.py
@@ -12,3 +12,17 @@ def get_sheeta_class(url: str) -> SheetaChannel|SheetaVideo|None:
         return SheetaChannel(url)
     else:
         return None
+
+def get_sheeta_class_type(url: str) -> str|None:
+    """
+    Get the type of the Sheeta class based on the URL.
+
+    Args:
+        url (str): URL of the video
+
+    Returns:
+        str: Type of the Sheeta class (video, live, channel)
+    """
+    sheeta_class_obj = Sheeta(url)
+    sheeta_class_obj.set_site_settings()
+    return sheeta_class_obj.type
